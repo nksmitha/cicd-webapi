836a2a5551ab54639028fc04c95dee7174fe66f8
smita <smithakagalkar@gmail.com>
2026-05-25 04:09:44 -0700

    added one moer methid

--- cicd-webapi/Controllers/WeatherForecastController.cs
+++ cicd-webapi/Controllers/WeatherForecastController.cs
@@ -22,5 +22,17 @@ namespace cicd_webapi.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet(Name = "GetWeatherForecast2")]
+        public IEnumerable<WeatherForecast> GetWeatherForecast2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
